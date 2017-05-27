adc7268d11e2b86e345abf63d858f0c41523eff1
Yang <zhang.yang.frank@gmail.com>
2017-05-27 16:33:36 -0400

    streaming test

--- playground/tweet.py
+++ playground/tweet.py
@@ -1,17 +1,24 @@
 import tweepy
 
-consumer_key = '8U3bzFLL0mJroJE0XvORNzYFR'
-consumer_secret = '0WNDIYOHnaDb5qA0woOFRvmXz5qGT8ti5SFlmEncCZGyW4pgF4'
-access_token = '1869401612-NgDAp9BHpbftOAz9oDmg6Z66zOldxZjBZ4dnTqp'
-access_token_secret = 'Z2IyFbFtQDFSvBtC8TbQa1Wqnv8t28UYdE6yad1x7YXFo'
+# override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+if __name__ == "__main__":
+    consumer_key = '8U3bzFLL0mJroJE0XvORNzYFR'
+    consumer_secret = '0WNDIYOHnaDb5qA0woOFRvmXz5qGT8ti5SFlmEncCZGyW4pgF4'
+    access_token = '1869401612-NgDAp9BHpbftOAz9oDmg6Z66zOldxZjBZ4dnTqp'
+    access_token_secret = 'Z2IyFbFtQDFSvBtC8TbQa1Wqnv8t28UYdE6yad1x7YXFo'
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+    myStream.filter(track=['memorialday'])
+
 
 # user = api.get_user('AlgoricZhang')
 # print(user.screen_name)
